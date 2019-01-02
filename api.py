fbac0b46072d72f242cad52f6824087f7a1c7a0d
Mark Murnane <mark@hackafe.net>
2019-01-01 20:16:23 -0500

    Updated slack message

--- api.py
+++ api.py
@@ -154,7 +154,7 @@ def api_pour():
             if not payments.bill_coldbrew(account):
                 return jsonify({"success": False, "error": "Failed to authorize transaction with Stripe."})
         cursor.execute("INSERT INTO transactions (account, amount, note, timestamp) VALUES (%s, %s, %s, %s)", (str(data['account']), str(data['amount']), data['note'], timestamp))
-        slack.poured()
+        slack.postText("{} poured a drink.".format(account['name']))
         return jsonify({"success": True})
 
 @app.route("/api/tapstate", methods=['GET', 'POST'])
